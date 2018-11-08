a62e3a9fe82db1d54ff0fec81aceb6025c7ce386
Przemek Peron <przemek@redkorn.pl>
2018-11-08 15:58:12 +0100

    Updated Changelog

--- src/AmazonCore.php
+++ src/AmazonCore.php
@@ -443,14 +443,6 @@ abstract class AmazonCore
                 $this->marketplaceId = $store[$s]['marketplaceId'];
             }
 
-            if (array_key_exists('authToken', $store[$s]) && !empty($store[$s]['authToken'])) {
-                $this->options['MWSAuthToken'] = $store[$s]['authToken'];
-            }
-
-            if (array_key_exists('marketplaceId', $store[$s]) && !empty($store[$s]['marketplaceId'])) {
-                $this->marketplaceId = $store[$s]['marketplaceId'];
-            }
-
         } else {
             $this->log("Store $s does not exist!", 'Warning');
             throw new Exception("Store $s does not exist!");
